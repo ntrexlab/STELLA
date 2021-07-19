19d67b8c74017b5797e9c6c744cbbc7358f2f4eb
ntrexlab <85467544+ntrexlab@users.noreply.github.com>
2021-07-19 10:53:32 +0900

    Update create_udev_rules.sh

--- stella_bringup/create_udev_rules.sh
+++ stella_bringup/create_udev_rules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "remap the devices serial port(ttyUSBX) to  ydlidar, AHRS, Motordriver, Bluetooth"
+echo "remap the devices serial port(ttyUSBX, ttySX) to  ydlidar, AHRS, Motordriver, Bluetooth"
 echo "devices usb connection as /dev/YDLIDAR, /dev/AHRS, /dev/MW, /dev/BT  , check it using the command : ls -l /dev|grep -e ttyUSB -e ttyS0"
 echo "start copy stella.rules to  /etc/udev/rules.d/"
 echo "`rospack find stella_bringup`/stella.rules"
